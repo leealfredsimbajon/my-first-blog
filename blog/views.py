623fba3ec28d866ec009232f1e7853d44c933fad
Lee Alfred <leealfred.simbajon@g.msuiit.edu.ph>
2026-01-23 10:25:09 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)  # Fetch the post or show 404
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
